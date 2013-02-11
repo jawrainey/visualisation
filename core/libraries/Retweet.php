f39f5d732487880adbc02e5cd210016b183d15ad
Jay Rainey <jawrainey@gmail.com>
2013-02-11 13:04:30 +0000

    not functional
    Just uploaded some files to make it easier for others to connect to db
    etc. My part is not fully functional (No need to test)!

--- core/libraries/Retweet.php
+++ core/libraries/Retweet.php
@@ -1,6 +1,4 @@
 <?php
-require 'Database.php';
-ini_set("memory_limit","800M"); // needed if outputing entire dataset to screen.
 /**
 * Obtain the most re-tweeted tweet from a given dataset.
 */
@@ -18,7 +16,7 @@ class Retweet
      *
      * @return the most repeated sentence in a given column.
      */
-    public function most_retweet()
+    public function most_retweet($hashtags = False)
     {
         //cosmos holds the database rows in an array - thus it's an array of arrays
         $cosmos = $this->db->select('SELECT * FROM cosmos');
@@ -41,17 +39,35 @@ class Retweet
      */
     public function getName($tweet)
     {
-        preg_match('/@([A-Za-z0-9_]{1,15})/', $tweet, $matches);
-        return $matches[0];
+        preg_match('/@([A-Za-z0-9_]{1,15})/', call_user_func($tweet), $username);
+        return $username[0];
+    }
+
+    /*
+     * 
+     *
+     * @return ...?
+     */    
+    public function graphit()
+    {
+        $retweets = array();
+        $cosmos = $this->db->select('SELECT * FROM cosmos');
+        $mostrt = $this->most_retweet();
+
+        foreach ($cosmos as $row)
+        {
+            if (preg_match('/'.$mostrt.'/', $row['tweet_text']))
+            {
+                array_push($retweets, array('datetime' => date('Y-m-d H:i:s',$row['timestamp']), 'location' => $row['location']));
+            }
+        }
+
+        $origin = $retweets[0]['datetime'];
+        
+        foreach ($retweets as $array)
+        {
+            $next_datetime = $array[0][0];
+            $res = $next_datetime - $origin;
+        }
     }
 }
-
-$db = new Database();
-$rt = new Retweet($db);
-
-Echo "<pre>";
-print_r($rt->most_retweet());
-Echo "</br>";
-print_r($rt->getName());
-Echo "</pre>";
-
--- index.php
+++ index.php
@@ -5,21 +5,50 @@
   <title>Home &middot; Group Seven</title>
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <link rel="stylesheet" href="./css/style.css" />
+  <script src="./js/d3.js"></script>
+  <script src="./js/jquery.js"></script>
+  <script src="./js/arbor.js"></script>
+  <script src="./js/arbor-tween.js"></script>
+  <script src="./js/renderer.js"></script>
+  <script src="./js/graphics.js"></script>
 </head>
 <body>
 <?php
-    
-    $numbers = array(1,2,3,4,5,6,7,8,9);
-    foreach ($numbers as $num)
-    {
-        if ($num % 2 != 0)
-        {
-            echo 'It is an odd number: ' + $num;
-        }
-    }
+
+//ini_set("memory_limit","800M"); // needed if outputing entire dataset to screen.
+//require 'core/libraries/Database.php';
+//require 'core/libraries/Retweet.php';
+
+//$db = new Database();
+//$rt = new Retweet($db);
+
+Echo '<pre>';
+//print_r($rt->graphit());
+Echo '</pre>';
 
 ?>
+<?php 
+//<script>
+    //var dataa = <?php echo json_encode($rt->graphit()); ;
+//</script>
+?>
+<canvas id="viewport" width="1200" height="500"></canvas>
+<script src="..../js/main.js"></script>
+<script>
+var sys = arbor.ParticleSystem();
+sys.parameters({stiffness:900, repulsion:2000, gravity:true, dt:0.015})
+sys.renderer = Renderer("#viewport");
 
+var origin = sys.addNode('Animals',{'color':'red','shape':'dot','label':'ORG'});
+var retweet = sys.addNode('dog',{'color':'blue','shape':'dot','label':'FAR'});
+var rt = sys.addNode('cat',{'color':'blue','shape':'dot','label':'CLOSE'});
+var rt6 = sys.addNode('cast',{'color':'blue','shape':'dot','label':'CLOSE'});
+var rt2 = sys.addNode('caat',{'color':'blue','shape':'dot','label':'CLOSE'});
+sys.addEdge(origin, retweet, 2);
+sys.addEdge(origin, rt, 0.5);
+sys.addEdge(origin, rt6, 1);
+sys.addEdge(origin, rt2, 1);
+</script>
 </body>
 </html>
 
