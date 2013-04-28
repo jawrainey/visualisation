7e7764baf782e7b0e3e04732de5fd112bdfad8b2
Jay Rainey <jawrainey@gmail.com>
2013-04-28 21:32:47 +0100

    Added database and email files
    Removed database and email files from history to ensure sensitive data
    wasn't leaked, and pushed empty files to allow the project to be
    configured as others desire.

---
+++ application/config/email.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Email
+| -------------------------------------------------------------------------
+| This file lets you define parameters for sending emails.
+| Please see the user guide for info:
+|
+| http://ellislab.com/codeigniter/user-guide/libraries/email.html
+|
+*/
+$config['protocol']     ='';
+$config['smtp_host']    ='';
+$config['smtp_port']    ='';
+$config['smtp_timeout'] ='';
+$config['smtp_user']    ='';
+$config['smtp_pass']    ='';
+
+$config['mailtype']     = 'html';
+$config['charset']      = 'utf-8';
+$config['newline']      = "\r\n";
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */ 
